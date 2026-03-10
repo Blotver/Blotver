357c46dc8671db4ba7a81dd57ffd0212e4d49d65
Blotver <matesitobot@gmail.com>
2026-03-10 20:22:11 -0300

    children

--- bot.js
+++ bot.js
@@ -444,17 +444,8 @@ app.post("/api/widgets/:id/test", isAuthenticated, async (req, res) => {
     width: widget.data.width,
     height: widget.data.height,
 
-    images: images.map(i => ({
-      ...i.data,
-      parentX: widget.data.x,
-      parentY: widget.data.y
-    })),
-
-    texts: texts.map(t => ({
-      ...t.data,
-      parentX: widget.data.x,
-      parentY: widget.data.y
-    })),
+    images: images.map(i => i.data),
+    texts: texts.map(t => t.data),
 
     overlayText: widget.data.overlayText || "",
     animationIn: widget.data.animationIn || "fade",
